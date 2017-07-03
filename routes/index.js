dddf1a0630700de7279e1df7c185bbf711c21961
Kyle E. Mitchell <kyle@kemitchell.com>
2017-07-02 18:41:22 -0700

    Serve stylesheets

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,17 @@
+var serveFile = require('./serve-file')
+var path = require('path')
+var send = require('send')
+
 var routes = module.exports = require('http-hash')()
 
 routes.set('/', require('./homepage'))
+
+staticFile('normalize.css')
+staticFile('styles.css')
+
+function staticFile (file) {
+  var filePath = path.join(__dirname, '..', 'static', file)
+  routes.set('/' + file, function (configuration, request, response) {
+    send(request, filePath).pipe(response)
+  })
+}
