a308be54db1f6c83e25e2af786538ce6d78582a9
Kyle E. Mitchell <kyle@kemitchell.com>
2017-07-02 18:43:36 -0700

    Rename index.html to homepage.html

--- routes/homepage.js
+++ routes/homepage.js
@@ -2,7 +2,7 @@ var internalError = require('./internal-error')
 var readTemplate = require('./read-template')
 
 module.exports = function (configuration, request, response) {
-  readTemplate('index.html', function (error, html) {
+  readTemplate('homepage.html', function (error, html) {
     /* istanbul ignore if */
     if (error) {
       internalError.apply(this, arguments)
