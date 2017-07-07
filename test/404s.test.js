35db7640a29b9d42f9a4a1e6c4e8ba396f9b34ce
Kyle E. Mitchell <kyle@kemitchell.com>
2017-07-07 13:43:04 -0700

    Test 404 responses

---
+++ test/404s.test.js
@@ -0,0 +1,78 @@
+var concat = require('./concat')
+var http = require('http')
+var server = require('./server')
+var tape = require('tape')
+
+var IMPROBABLE_CAPABILITY = 'a'.repeat(64)
+
+tape.test('GET /${nonexistent}', function (test) {
+  server(function (port, closeServer) {
+    http.request({
+      port: port,
+      path: '/nonexistent'
+    })
+      .once('response', function (response) {
+        test.equal(
+          response.statusCode, 404,
+          'responds 404'
+        )
+        test.end()
+        closeServer()
+      })
+      .end()
+  })
+})
+
+tape.test('GET /view/${invalid}', function (test) {
+  server(function (port, closeServer) {
+    http.request({
+      port: port,
+      path: '/view/' + IMPROBABLE_CAPABILITY
+    })
+      .once('response', function (response) {
+        test.equal(
+          response.statusCode, 404,
+          'responds 404'
+        )
+        test.end()
+        closeServer()
+      })
+      .end()
+  })
+})
+
+tape.test('GET /cancel/${invalid}', function (test) {
+  server(function (port, closeServer) {
+    http.request({
+      port: port,
+      path: '/cancel/' + IMPROBABLE_CAPABILITY
+    })
+      .once('response', function (response) {
+        test.equal(
+          response.statusCode, 404,
+          'responds 404'
+        )
+        test.end()
+        closeServer()
+      })
+      .end()
+  })
+})
+
+tape.test('GET /countersign/${invalid}', function (test) {
+  server(function (port, closeServer) {
+    http.request({
+      port: port,
+      path: '/countersign/' + IMPROBABLE_CAPABILITY
+    })
+      .once('response', function (response) {
+        test.equal(
+          response.statusCode, 404,
+          'responds 404'
+        )
+        test.end()
+        closeServer()
+      })
+      .end()
+  })
+})
