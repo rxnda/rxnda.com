94a56bf396bd8b4268beb40c634b7e3562e033c2
Kyle E. Mitchell <kyle@kemitchell.com>
2017-07-16 12:12:46 -0700

    Revise draft-form warning

--- util/draft-warning.js
+++ util/draft-warning.js
@@ -2,9 +2,9 @@ module.exports = function draftWarning (edition) {
   if (edition.edition.endsWith('d')) {
     return `
 <p class=warning>
-  This is a draft form, not a final edition.  Unless you
-  have a specific reason to prefer this draft, you should
-  use a final edition instead.
+  This is a draft form, not a final edition.  You should
+  use a final edition unless you have a specific reason
+  to prefer this draft.
 </p>`
   } else {
     return ''
