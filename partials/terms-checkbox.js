ceef4b07aeb88b240ea887b9ebd1a64d3188a4e0
Kyle E. Mitchell <kyle@kemitchell.com>
2017-07-21 20:06:43 -0700

    Refactor terms checkbox partial

--- partials/terms-checkbox.js
+++ partials/terms-checkbox.js
@@ -1,7 +1,8 @@
+var html = require('../routes/html')
 var paragraphs = require('./paragraphs')
 
 module.exports = function (errors) {
-  return `
+  return html`
 <section>
   <h3>Terms of Service</h3>
   <label for=terms>
@@ -9,6 +10,6 @@ module.exports = function (errors) {
     Check this box to show that you accept this site&rsquo;s
     <a href=/terms target=_blank>terms of service</a>.
   </label>
-  ${errors ? paragraphs(errors, 'error') : ''}
+  ${errors && paragraphs(errors, 'error')}
 </section>`
 }
