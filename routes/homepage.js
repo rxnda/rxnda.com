569d31fefe60e4f914cebfed4e6e2f6834bc3d1a
Kyle E. Mitchell <kyle@kemitchell.com>
2017-08-10 14:51:16 -0700

    Tweak homepage link to form picker

--- routes/homepage.js
+++ routes/homepage.js
@@ -21,7 +21,7 @@ ${nav()}
   </p>
   <ol>
     <li>
-      Visit <a href=/send>the form picker</a>.  Answer a
+      <a href=/send>Visit the form picker</a>.  Answer a
       few questions about your needs and click the big button
       to find your form.  Have your lawyer review it to
       make sure it meets your needs.
