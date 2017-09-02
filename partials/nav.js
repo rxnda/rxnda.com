db66dce1e0c42f98bdb116bab28d2c8596f0dc0a
Kyle E. Mitchell <kyle@kemitchell.com>
2017-09-01 17:28:51 -0700

    Replace welcome link with register link in <nav>

--- partials/nav.js
+++ partials/nav.js
@@ -2,10 +2,10 @@ module.exports = function () {
   return `
 <nav role=navigation>
   <ul>
-    <li><a href=/>Welcome</a></li>
     <li><a href=/send>Send</a></li>
     <li><a href=/pricing>Pricing</a></li>
     <li><a href=/forms>Forms</a></li>
+    <li><a href=/register>Register</a></li>
     <li><a href=/questions>Q&amp;A</a></li>
     <li><a href=/future>Future</a></li>
     <li><a href=/help>Help</a></li>
