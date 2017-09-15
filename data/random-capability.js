0148af8df499d77ae059911e881a327d7cea42e3
Kyle E. Mitchell <kyle@kemitchell.com>
2017-09-15 10:24:29 -0700

    Use sodium to generate random bytes

--- data/random-capability.js
+++ data/random-capability.js
@@ -1,8 +1,7 @@
-var readRandom = require('read-random')
+var sodium = require('sodium-native')
 
 module.exports = function randomCapability (callback) {
-  readRandom(32, function (error, bytes) {
-    if (error) return callback(error)
-    callback(null, bytes.toString('hex'))
-  })
+  var returned = Buffer.alloc(32)
+  sodium.randombytes_buf(returned)
+  callback(null, returned.toString('hex'))
 }
